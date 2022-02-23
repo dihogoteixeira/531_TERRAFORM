21e060b7f7be3d9e9e9631afac1eac0ebf23ac8b
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-02-23 18:06:20 -0300

    aula-05

---
+++ AULA_05/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.11.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "4linuxteam"
+    prefix = "terraform/state"
+  }
+}
+provider "google" {
+  # Configuration options
+}
