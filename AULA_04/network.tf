d814af1c8de071cf4e71905d0f4dc6e3e6d027a0
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-02-22 21:22:37 -0300

    aula-04 adds resources

---
+++ AULA_04/network.tf
@@ -0,0 +1,4 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "bob"
+  auto_create_subnetworks = false
+}
---
+++ AULA_04/subnetworks.tf
@@ -0,0 +1,13 @@
+resource "google_compute_subnetwork" "sub-1" {
+  name          = "sub-1"
+  ip_cidr_range = "10.10.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "sub-2" {
+  name          = "sub-2"
+  ip_cidr_range = "10.20.0.0/16"
+  region        = "asia-east1"
+  network       = google_compute_network.vpc_network.id
+}
