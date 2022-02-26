77c3af7b93bcf97a104da1471ce0c6da896ded85
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-02-25 22:34:00 -0300

    Demo: Instanciando local modules

--- AULA_07/outputs.tf
+++ AULA_07/outputs.tf
@@ -2,6 +2,7 @@
 output "name" {
     value = module.group-jenkins.name
 }
+
 output "external_ip" {
     value = module.group-jenkins.external_ip
 }
@@ -10,6 +11,7 @@ output "external_ip" {
 output "name" {
     value = module.group-web.name
 }
+
 output "external_ip" {
     value = module.group-web.external_ip
 }
@@ -18,6 +20,7 @@ output "external_ip" {
 output "name" {
     value = module.group-gitlab.name
 }
+
 output "external_ip" {
     value = module.group-gitlab.external_ip
 }
