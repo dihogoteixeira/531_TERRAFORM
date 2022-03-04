f5f62c535ec572e72caca7b1871f86d653482933
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-03-03 22:37:55 -0300

    README module it demonstracao da marcacao mark down

---
+++ AULA_09/mod-gcp-it/README_DEMO.md
@@ -0,0 +1,35 @@
+# Texto
+##  Texto
+###  Texto
+
+Exemplo `>=0.13`
+
+```python
+var = "value"
+```
+
+```hcl
+variable "tal" {
+ type = string
+}
+```
+
+```sh
+echo $EXEMPLO
+```
+
+1. texto indice 1
+2. texto indice 2
+3. texto indice 3
+
+- texto indice 1
+- texto indice 2
+- texto indice 3
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| auto | descricao   | bool |  false  |  no      |
+
+[terraform](https://registry.terraform.io)
+[!terraform]("./../../../LINKS/img/531_TF_LOGO.png")
+
--- AULA_09/mod-gcp-it/main.tf
+++ AULA_09/mod-gcp-it/main.tf
@@ -29,10 +29,12 @@ resource "google_compute_instance_template" "this" {
   network_interface {
     network    = var.network
     subnetwork = var.subnetwork
-    # Como teste, habilitar ips externos nas maquinas tambem
-    # Mundo real, nao seria necessario, o loadbalance iria bater direto nas instancias
-    # e caso fosse necessario efetuar SSH nas maquinas bastava criar uma instancia na mesma rede com 
-    # acesso de rede externo
+    /*
+      Como teste, habilitar ips externos nas maquinas tambem
+      Mundo real, nao seria necessario, o loadbalance iria bater direto nas instancias
+      e caso fosse necessario efetuar SSH nas maquinas bastava criar uma instancia na mesma rede com 
+      acesso de rede externo
+    */
     access_config {
       // Ephemeral IP
     }
